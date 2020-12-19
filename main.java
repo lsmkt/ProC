5f49b343c9cd5d5cc5428c6c226b97367b772c60
lsmkt <75500372+lsmkt@users.noreply.github.com>
2020-12-19 13:46:44 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -4,86 +4,132 @@ import java.util.Random;
 public class main {
 	
 	
-	
+	public static int[] rawMaterials = new int [110];
 	
 	public static void main(String[] args) throws IOException {
 		
 		
-		int orders, inventory=15, incomingBeer=15, unfilledOrders=0, previousIN=0;
+		int orders, inventory1,inventory2=0, incomingBeer=15, unfilledOrders=0;
+		
+		rawMaterials[0]=15;
+		rawMaterials[1]=15;
 		
 		
-		for(int x=1; x<101; x++) {
+		for(int x=2; x<102; x++) {
+			
+			inventory1=inventory2;
 		
 			orders=random();
 			
-		
-			previousIN=inventory;
+			incomingBeer=rawMaterials[x-2];
 			
-			inventory=inventory-orders-unfilledOrders+incomingBeer;
+			inventory2=inventory1-orders-unfilledOrders+incomingBeer;
 			
-			
-			unfilledOrders=unfilledOrders+orders-previousIN;
-			
+			unfilledOrders=unfilledOrders+orders-inventory1-incomingBeer;
 			
 			if(unfilledOrders<0) {
 				unfilledOrders=0;
 			}
+			System.out.println();
+			inFunction(inventory2, unfilledOrders, x);
 			
-			
-			
-			incomingBeer=inFunction(inventory, unfilledOrders, previousIN);
-			
-			if(inventory<0) {
-				inventory=0;
+			if(inventory2<0) {
+				inventory2=0;
 			}
 			
-			System.out.format("WEEK %d has %d customers and %d inventory left\n", x, orders, inventory );
-			System.out.println(unfilledOrders);
-			
+			System.out.format("week %d \nhas %d customers and %d inventory left\n", x-1, orders, inventory2 );
+			System.out.println("unfilled "+unfilledOrders);
+			System.out.println("ordered "+rawMaterials[x]);
 		}
 		
 	}
 	
 	public static int random () {
 		
-		int x, orders=0;
+		int orders=0;
 		Random rand = new Random ();
 		int randomInt, upperBound=101;
 		
 			randomInt=rand.nextInt(upperBound);
 						
 			if(randomInt<=7) {
-				//System.out.println(" 9");
 				orders=9;
 			}
 			
-			if(randomInt>7 && randomInt<93) {
-				//System.out.println(" 15");
+			if(randomInt>7 && randomInt<=93) {
 				orders=15;
 			}
 			
 			if(randomInt>93) {
-				//System.out.println(" 21");
 				orders=21;
 			}
 		
 		return orders;
 	}
 	
-	public static int inFunction (int inventory, int unfilled, int previousIN) {
+	public static void inFunction (int inventory, int unfilled, int week) {
 		
-		int newOrder=15;
 		
-		if(inventory>=10 && unfilled==0) {
-			newOrder=10;
-		}
 		
-		if(unfilled>=10) {
-			newOrder=25;
+		if(inventory > 10 && inventory< 30 && unfilled == 0) { //too much, order less
+			
+			//for the next three weeks
+			
+			rawMaterials[week]=rawMaterials[week-1]-5;
+		//	rawMaterials[week+1]=rawMaterials[week-1]-5;
+		//	rawMaterials[week+2]=rawMaterials[week-1]-5;
+			System.out.println("1");
 			
 		}
 		
-		return newOrder;
+		else if(inventory >= 30 && unfilled == 0) { //too much, order less
+			
+			//for the next three weeks
+			
+			rawMaterials[week]=0;
+		//	rawMaterials[week+1]=0;
+		//	rawMaterials[week+2]=0;
+			System.out.println("2");
+			
+		}
+		
+		else if (inventory < 10 && unfilled == 0) {
+			rawMaterials[week]=15;
+			System.out.println("3");
+		}
+		
+		else if(unfilled > 0 && unfilled < 5)  { //back ordered. order more
+			
+			rawMaterials[week]=(int) (rawMaterials[week-1]+unfilled*0.25);
+			//rawMaterials[week+1]=rawMaterials[week-1]+5;
+			//rawMaterials[week+2]=rawMaterials[week-1]+5;
+			System.out.println("4");
+			
+		}
+		
+		else if(unfilled > 4) { //very back ordered. order a lot more
+			
+			rawMaterials[week]=(int) (rawMaterials[week-1]+unfilled*0.5);
+			//rawMaterials[week+1]=rawMaterials[week-1]+10;
+			//rawMaterials[week+2]=rawMaterials[week-1]+10;
+	
+			System.out.println("5");
+			
+		}
+		
+		else{
+			
+			rawMaterials[week]=rawMaterials[week-1];
+			System.out.println("6");
+			
+		}
+		
+		if(rawMaterials[week] < 0) {
+			
+			rawMaterials[week]=0;
+			
+		}
+		
 	}
 	
 	
